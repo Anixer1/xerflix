a8e7b63867fd2d259997797cc1d9a230d5af585b
Anixer1 <165531263+Anixer1@users.noreply.github.com>
2024-04-25 21:00:48 +0800

    Update header.jsx

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
 						href={"/"}
 						style={{ textDecoration: "none", color: "white" }}
 					>
-						<p>Dramalama</p>
+						<p>XerFlix</p>
 					</Link>
 				</div>
 				<div className={styles.right}>
